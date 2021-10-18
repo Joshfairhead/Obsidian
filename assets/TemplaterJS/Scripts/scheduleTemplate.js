584314fcd2bbde641d45110d2da01168f89765d8
Josh Fairhead <hello@joshafairhead.com>
2021-10-18 23:20:43 +0200

    vault backup: 2021-10-18 23:20:43
    Affected files:
    
    assets/TemplaterJS/Scripts/scheduleTemplate.js
    assets/TemplaterJS/Templates/Sunday.md

--- assets/TemplaterJS/Scripts/scheduleTemplate.js
+++ assets/TemplaterJS/Scripts/scheduleTemplate.js
@@ -3,7 +3,7 @@ function scheduleTemplate(tp) {
       case 1: return tp.file.include("[[Monday]]")
       case 2: return tp.file.include("[[Tuesday]]")
       case 3: return tp.file.include("[[Wednesday]]")
-      case 4: return tp.file.include("[[Thrusday]]")
+      case 4: return tp.file.include("[[Thursday]]")
       case 5: return tp.file.include("[[Friday]]")
       case 6: return tp.file.include("[[Saturday]]")
       case 7: return tp.file.include("[[Sunday]]")
