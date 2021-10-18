b16ebf2d0d69a23b9ea210f4b4b853e81b0f5532
Josh Fairhead <hello@joshafairhead.com>
2021-10-18 22:42:43 +0200

    vault backup: 2021-10-18 22:42:43
    Affected files:
    
    assets/TemplaterJS/Scripts/scheduleTemplate.js

--- assets/TemplaterJS/Scripts/scheduleTemplate.js
+++ assets/TemplaterJS/Scripts/scheduleTemplate.js
@@ -1,11 +1,12 @@
 function scheduleTemplate(tp) {
     switch (parseInt(moment(tp.file.title).format('d'))) {
-      case 1: return tp.file.include("[[Schedule - 01 Monday]]")
-      case 2: return tp.file.include("[[Schedule - 02 Tuesday]]")
-      case 3: return tp.file.include("[[Schedule - 03 Wednesday]]")
-      case 4: return tp.file.include("[[Schedule - 04 Thrusday]]")
-      case 5: return tp.file.include("[[Schedule - 05 Friday]]")
-      case 6:
+      case 1: return tp.file.include("[[Monday]]")
+      case 2: return tp.file.include("[[Tuesday]]")
+      case 3: return tp.file.include("[[Wednesday]]")
+      case 4: return tp.file.include("[[Thrusday]]")
+      case 5: return tp.file.include("[[Friday]]")
+      case 6: return tp.file.include("[[Saturday]]")
+      case 7: return tp.file.include("[[Sunday]]")
       case 0: return tp.file.include("[[Schedule - Weekend]]")
       default: return tp.file.include("[[Schedule - +Weekday]]")
     }
